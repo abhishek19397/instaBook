cc2752674595fa928fbc71eac55d3fcbabc520f7
abhishek19397 <abhishek.tiwari19397@gmail.com>
2020-07-01 12:21:15 +0530

    Config

--- config/firebase.js
+++ config/firebase.js
@@ -3,14 +3,14 @@ import firebase from "firebase";
 require("firebase/firestore");
 
 const config = {
-  apiKey: "AIzaSyCz8Dc0Y_vyTKsyv4hmgNPUnnFXjqPk5NY",
-  authDomain: "feedbook-a155f.firebaseapp.com",
-  databaseURL: "https://feedbook-a155f.firebaseio.com",
-  projectId: "feedbook-a155f",
-  storageBucket: "feedbook-a155f.appspot.com",
-  messagingSenderId: "362069413229",
-  appId: "1:362069413229:web:5136fe5dc3bab163531e64",
-  measurementId: "G-XK4D4EV92Y",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 firebase.initializeApp(config);
